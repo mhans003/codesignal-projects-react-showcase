78fd9f526514b96e338c03574eeaf0e5dec8f942
mhans003 <michaeledwardhanson@gmail.com>
2021-09-12 11:51:53 -0700

    Added solution to Is Unstable Pair

--- codesignal-projects-react/src/solution-code/challenges_g_j.js
+++ codesignal-projects-react/src/solution-code/challenges_g_j.js
@@ -1074,6 +1074,40 @@ const challenges_g_j = {
                 }
             ]
         }
+    },
+    isUnstablePair: {
+        instructions: `Some file managers sort filenames taking into account case of the letters, others compare strings as if all of the letters are of the same case. That may lead to different ways of filename ordering.
+
+        Call two filenames an unstable pair if their ordering depends on the case.
+        
+        To compare two filenames a and b, find the first position i at which a[i] ≠ b[i]. If a[i] < b[i], then a < b. Otherwise a > b. If such position doesn't exist, the shorter string goes first.
+        
+        Given two filenames, check whether they form an unstable pair.`,
+        codeOutput: 
+        `function isUnstablePair(filename1, filename2) {
+            //See if there is a difference between the file names as is and the file names made lowercase.
+            return filename1 > filename2 === filename1.toLowerCase() > filename2.toLowerCase() ? 
+            false : 
+            true;
+        }`,
+        code: function isUnstablePair(filename1, filename2) {
+            //See if there is a difference between the file names as is and the file names made lowercase.
+            return filename1 > filename2 === filename1.toLowerCase() > filename2.toLowerCase() ? 
+            false : 
+            true;
+        },
+        arguments: {
+            descriptions: [
+                {
+                    text: "Filename 1 (String)",
+                    type: "String"
+                },
+                {
+                    text: "Filename 2 (String)",
+                    type: "String"
+                }
+            ]
+        }
     }
 };
 
