b2800f2d42a84048cb4aeea346d03fce8f4be4fc
mhans003 <michaeledwardhanson@gmail.com>
2021-09-13 16:40:02 -0700

    Added solution to Number of Clans

--- codesignal-projects-react/src/pages/Solutions.js
+++ codesignal-projects-react/src/pages/Solutions.js
@@ -140,6 +140,7 @@ const Solutions = () => {
                 <option value="newYearCelebrations">New Year Celebrations</option>
                 <option value="noIfsNoButs">No Ifs No Buts</option>
                 <option value="nthNumber">Nth Number</option>
+                <option value="numberOfClans">Number of Clans</option>
             </select>
             <Solution challengeName={challengeName}/>
         </>
--- codesignal-projects-react/src/solution-code/challenges_k_n.js
+++ codesignal-projects-react/src/solution-code/challenges_k_n.js
@@ -1593,6 +1593,76 @@ const challenges_k_n = {
                 }
             ]
         }
+    },
+    numberOfClans: {
+        instructions: `Let's call two integers A and B friends if each integer from the array divisors is either a divisor of both A and B or neither A nor B. If two integers are friends, they are said to be in the same clan. How many clans are the integers from 1 to k, inclusive, broken into?`,
+        codeOutput: 
+        `function numberOfClans(divisors, k) {
+            //Loop through every number 1-k. 
+            let factorCombos = {};
+            for(let i = 1; i <= k; i++) {
+                //For each of them, find out all numbers from divisors that are factors.
+                let factors = {};
+                for(let j = 0; j < divisors.length; j++) {
+                    if(i % divisors[j] === 0 && !factors[divisors[j]]) {
+                        factors[divisors[j]] = true;
+                    }
+                }
+                //Combine all factors 
+                let factorString = "";
+                for(let factor in factors) {
+                    factorString += factor + " ";
+                }
+                factorString = factorString.length > 0 ? factorString.trim() : "none";
+                //Add this combination of factors to the factorCombos map.
+                if(factorCombos[factorString]) {
+                    factorCombos[factorString]++;
+                } else {
+                    factorCombos[factorString] = 1;
+                }
+            }
+            //Return total numbers of different combinations
+            return Object.keys(factorCombos).length;
+        }`,
+        code: function numberOfClans(divisors, k) {
+            //Loop through every number 1-k. 
+            let factorCombos = {};
+            for(let i = 1; i <= k; i++) {
+                //For each of them, find out all numbers from divisors that are factors.
+                let factors = {};
+                for(let j = 0; j < divisors.length; j++) {
+                    if(i % divisors[j] === 0 && !factors[divisors[j]]) {
+                        factors[divisors[j]] = true;
+                    }
+                }
+                //Combine all factors 
+                let factorString = "";
+                for(let factor in factors) {
+                    factorString += factor + " ";
+                }
+                factorString = factorString.length > 0 ? factorString.trim() : "none";
+                //Add this combination of factors to the factorCombos map.
+                if(factorCombos[factorString]) {
+                    factorCombos[factorString]++;
+                } else {
+                    factorCombos[factorString] = 1;
+                }
+            }
+            //Return total numbers of different combinations
+            return Object.keys(factorCombos).length;
+        },
+        arguments: {
+            descriptions: [
+                {
+                    text: "Array of Integers (No Brackets []; Form of 1,2,3...)",
+                    type: "NumberArray"
+                },
+                {
+                    text: "Integer k",
+                    type: "Number"
+                }
+            ]
+        }
     }
 };
 
