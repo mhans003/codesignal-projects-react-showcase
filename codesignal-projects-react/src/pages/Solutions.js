2050722905e46c8ccaf03d736d2984da6d54b40c
mhans003 <michaeledwardhanson@gmail.com>
2021-09-14 08:31:20 -0700

    Added solution to Replace Middle

--- codesignal-projects-react/src/pages/Solutions.js
+++ codesignal-projects-react/src/pages/Solutions.js
@@ -155,6 +155,7 @@ const Solutions = () => {
                 <option value="regularMonths">Regular Months</option>
                 <option value="removeArrayPart">Remove Array Part</option>
                 <option value="replaceAllDigitsRegExp">Replace All Digits RegExp</option>
+                <option value="replaceMiddle">Replace Middle</option>
             </select>
             <Solution challengeName={challengeName}/>
         </>
--- codesignal-projects-react/src/solution-code/challenges_o_r.js
+++ codesignal-projects-react/src/solution-code/challenges_o_r.js
@@ -671,6 +671,36 @@ const challenges_o_r = {
                 }
             ]
         }
+    },
+    replaceMiddle: {
+        instructions: `We define the middle of the array arr as follows:
+
+        if arr contains an odd number of elements, its middle is the element whose index number is the same when counting from the beginning of the array and from its end;
+        if arr contains an even number of elements, its middle is the sum of the two elements whose index numbers when counting from the beginning and from the end of the array differ by one.
+        Given array arr, your task is to find its middle, and, if it consists of two elements, replace those elements with the value of middle. Return the resulting array as the answer.`,
+        codeOutput: 
+        `function replaceMiddle(arr) {
+            return arr.length % 2 ?
+            arr :
+            arr.slice(0, Math.floor(arr.length / 2) - 1)
+            .concat(arr[Math.floor(arr.length / 2) - 1] + arr[Math.floor(arr.length / 2)])
+            .concat(arr.slice(Math.floor(arr.length / 2) + 1));
+        }`,
+        code: function replaceMiddle(arr) {
+            return arr.length % 2 ?
+            arr :
+            arr.slice(0, Math.floor(arr.length / 2) - 1)
+            .concat(arr[Math.floor(arr.length / 2) - 1] + arr[Math.floor(arr.length / 2)])
+            .concat(arr.slice(Math.floor(arr.length / 2) + 1));
+        },
+        arguments: {
+            descriptions: [
+                {
+                    text: "Array of Integers (No Brackets []; Form of 1,2,3...)",
+                    type: "NumberArray"
+                }
+            ]
+        }
     }
 };
 
