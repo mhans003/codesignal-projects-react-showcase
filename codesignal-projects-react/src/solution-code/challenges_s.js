7cde3bdb309065ff4f3f22a37062bce5c8c01eb4
mhans003 <michaeledwardhanson@gmail.com>
2021-09-15 18:58:32 -0700

    Added file for challenges_s

---
+++ codesignal-projects-react/src/solution-code/challenges_s.js
@@ -0,0 +1,38 @@
+//Include global BigInt (as comment) in order to prevent no-undef in this environment.
+/* global BigInt */
+
+const challenges_s = {
+    
+};
+
+export default challenges_s;
+
+/*
+
+Template:
+
+name: {
+    instructions: ``,
+    codeOutput: 
+    ``,
+    code: ,
+    arguments: {
+        descriptions: [
+            {
+                text: "",
+                type: ""
+            }
+        ]
+    }
+}
+
+Type List:
+Number
+String
+Number Array 
+String Array
+Number Array Array
+String Array Array 
+BooleanArrayArray
+
+*/
