3fbb04067e7e37e3b502412e0484d8d33c01b4bd
mhans003 <michaeledwardhanson@gmail.com>
2021-09-25 22:58:47 -0700

    Check if any value is NaN when an array is output. If so, throw an error message.

--- codesignal-projects-react/src/components/Solution.js
+++ codesignal-projects-react/src/components/Solution.js
@@ -138,7 +138,11 @@ const Solution = props => {
             //Get output from running function. If the function returns null or undefined, we know something was wrong with the inputs.
             try {
                 output = allChallenges[`${props.challengeName}`].code(...args) ?? "Something went wrong with the inputs.";
-                if(isNaN(output)) throw("Output was not a number. Try another input.");
+                if(output === isNaN) throw("Output was not a number. Try another input.");
+                //If the output was in array form, see if any value was NaN.
+                if(Array.isArray(output)) {
+                    if(output.includes(NaN)) throw("Output was not a number. Try another input.");
+                }
             } catch(error) {
                 output = `Error running code: ${error}`;
             }
