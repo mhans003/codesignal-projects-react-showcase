199eb6cd2754313f69150a0c3eb70ce55fbb01c4
mhans003 <michaeledwardhanson@gmail.com>
2021-10-09 15:25:32 -0700

    On Homepage, added links to Repos

--- codesignal-projects-react/src/pages/Landing.js
+++ codesignal-projects-react/src/pages/Landing.js
@@ -9,10 +9,16 @@ const Landing = () => {
                 <hr className="my-5"/>
                 <div className="col-lg-6 mx-auto">
                     <p className="lead mb-4">Browse my JavaScript solutions to coding challenges.</p>
-                    <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                    <div className="justify-content-center">
                         <Link to={"solutions"}>
-                            <button type="button" className="btn btn-info btn-lg px-4 gap-3">View Solutions</button>
+                            <button type="button" className="btn btn-info btn-lg px-4 mx-auto my-2 d-block">View Solutions</button>
                         </Link>
+                        <a href="https://github.com/mhans003/codesignal-projects-react-showcase" target="_blank" rel="noopener noreferrer">
+                            <button type="button" className="btn btn-secondary btn-lg px-4 mx-auto my-2 d-block">Project Repo <i className="fab fa-github"></i></button>
+                        </a>
+                        <a href="https://github.com/mhans003/algorithm-solutions-cs" target="_blank" rel="noopener noreferrer">
+                            <button type="button" className="btn btn-secondary btn-lg px-4 mx-auto my-2 d-block">Solutions Repo <i className="fab fa-github"></i></button>
+                        </a>
                     </div>
                 </div>
             </div>
