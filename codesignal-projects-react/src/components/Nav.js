818f660d900ae668eb5536332b81e4a104df806c
mhans003 <michaeledwardhanson@gmail.com>
2021-11-03 12:53:24 -0700

    Commenting out unused vars in Nav

--- codesignal-projects-react/src/components/Nav.js
+++ codesignal-projects-react/src/components/Nav.js
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
-import Landing from '../pages/Landing';
+//import { Link } from 'react-router-dom';
+//import Landing from '../pages/Landing';
 
 const Nav = () => {
     return (
