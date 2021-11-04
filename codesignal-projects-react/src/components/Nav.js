2f9539871bf0522be337fb8977838ce43c75b79b
mhans003 <michaeledwardhanson@gmail.com>
2021-11-04 14:16:01 -0700

    Updated links in Nav for deployment

--- codesignal-projects-react/src/components/Nav.js
+++ codesignal-projects-react/src/components/Nav.js
@@ -4,7 +4,7 @@
 const Nav = () => {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-info py-4">
-            <a className="navbar-brand ml-3" href="/">CodeSignal Solutions</a>
+            <a className="navbar-brand ml-3" href="/codesignal-projects-react-showcase">CodeSignal Solutions</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
@@ -14,7 +14,7 @@ const Nav = () => {
                         <a className="nav-link text-light" href="https://mhans003.github.io/portfolio/portfolio.html" target="_blank" rel="noopener noreferrer">Michael Hanson</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-light" href="/">Home</a>
+                        <a className="nav-link text-light" href="/codesignal-projects-react-showcase">Home</a>
                     </li>
                 </ul>
             </div>
