f7756acd175d644917879cfaf0871c32d85e573b
mhans003 <michaeledwardhanson@gmail.com>
2021-11-06 13:22:08 -0700

    Toggled links for deployment

--- codesignal-projects-react/src/pages/Landing.js
+++ codesignal-projects-react/src/pages/Landing.js
@@ -11,14 +11,16 @@ const Landing = () => {
                 <div className="col-lg-6 mx-auto">
                     <p className="lead mb-4" style={{fontSize:"1.4rem"}}>Browse my JavaScript solutions to coding challenges.</p>
                     <div className="justify-content-center">
-                        {/*
+                        
                         <Link to={"/solutions/codesignal-projects-react-showcase"}>
                             <button type="button" className="btn btn-info btn-lg px-4 mx-auto my-2 d-block">View Solutions</button>
                         </Link>
-                        */}
+                    
+                        {/*
                         <Link to={"solutions"}>
                             <button type="button" className="btn btn-info btn-lg px-4 mx-auto my-2 d-block">View Solutions</button>
                         </Link>
+                        */}
                         <a href="https://github.com/mhans003/codesignal-projects-react-showcase" target="_blank" rel="noopener noreferrer">
                             <button type="button" className="btn btn-secondary px-4 mx-auto my-2 d-block">Project Repo <i className="fab fa-github"></i></button>
                         </a>
