72628efbc3d0eed0c89dd6627791ec2ca88ad262
mhans003 <michaeledwardhanson@gmail.com>
2021-12-05 08:21:55 -0800

    Added solution to higherVersion to list

--- codesignal-projects-react/src/solution-code/challenges_g_j.js
+++ codesignal-projects-react/src/solution-code/challenges_g_j.js
@@ -111,6 +111,53 @@ const challenges_g_j = {
             ]
         }
     },
+    higherVersion: {
+        name: 'higherVersion',
+        instructions: `Given two version strings composed of several non-negative decimal fields separated by periods ("."), both strings contain equal number of numeric fields. Return true if the first version is higher than the second version and false otherwise.
+
+        The syntax follows the regular semver ordering rules:
+        
+        1.0.5 < 1.1.0 < 1.1.5 < 1.1.10 < 1.2.0 < 1.2.2
+        < 1.2.10 < 1.10.2 < 2.0.0 < 10.0.0
+        There are no leading zeros in any of the numeric fields, i.e. you do not have to handle inputs like 100.020.003 (it would instead be given as 100.20.3).`,
+        codeOutput: 
+        `function higherVersion(ver1, ver2) {
+            //If the strings are the same, return false;
+            if(ver1 === ver2) return false;
+            //Create array versions of each number as numbers for proper comparisons
+            let ver1split = ver1.split(".").map(string => Number(string));
+            let ver2split = ver2.split(".").map(string => Number(string));
+            //Return early when the appropriate condition is met.
+            for(let i = 0; i < ver1split.length; i++) {
+                if(ver2split[i] > ver1split[i]) return false;
+                if(ver1split[i] > ver2split[i]) return true;
+            }
+        }`,
+        code: function higherVersion(ver1, ver2) {
+            //If the strings are the same, return false;
+            if(ver1 === ver2) return false;
+            //Create array versions of each number as numbers for proper comparisons
+            let ver1split = ver1.split(".").map(string => Number(string));
+            let ver2split = ver2.split(".").map(string => Number(string));
+            //Return early when the appropriate condition is met.
+            for(let i = 0; i < ver1split.length; i++) {
+                if(ver2split[i] > ver1split[i]) return false;
+                if(ver1split[i] > ver2split[i]) return true;
+            }
+        },
+        arguments: {
+            descriptions: [
+                {
+                    text: "String of numbers/decimals as described above (e.g. 1.2.3)",
+                    type: "String"
+                },
+                {
+                    text: "String of numbers/decimals as described above (e.g. 1.2.3)",
+                    type: "String"
+                }
+            ]
+        }
+    },
     holiday: {
         name: 'holiday',
         instructions: `John Doe likes holidays very much, and he was very happy to hear that his country's government decided to introduce yet another one. He heard that the new holiday will be celebrated each year on the xth week of month, on weekDay.
