37390f6514367ed7a50ab871bf904debda54889f
mhans003 <michaeledwardhanson@gmail.com>
2022-01-22 11:43:59 -0800

    Updated root paths for a new deploy

--- codesignal-projects-react/src/App.js
+++ codesignal-projects-react/src/App.js
@@ -8,13 +8,13 @@ function App() {
     <Router>
       <Switch>
         
-        {/*
         <Route exact path="/codesignal-projects-react-showcase" component={Landing}/>
         <Route exact path="/solutions/codesignal-projects-react-showcase" component={Solutions}/>
-        */}
         
+        {/*
         <Route exact path="/" component={Landing}/>
         <Route exact path="/solutions" component={Solutions}/>
+        */}
         
       </Switch>
     </Router>
