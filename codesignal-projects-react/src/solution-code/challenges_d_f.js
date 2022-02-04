730d43394138f9c73ca345b135ec927b54497cf9
mhans003 <michaeledwardhanson@gmail.com>
2022-02-04 04:23:56 -0800

    Added solution to findLUSlength

--- codesignal-projects-react/src/solution-code/challenges_d_f.js
+++ codesignal-projects-react/src/solution-code/challenges_d_f.js
@@ -1208,6 +1208,47 @@ const challenges_d_f = {
             ]
         }
     },
+    findLUSlength: {
+        name: `findLUSlength`,
+        instructions: `Given two strings a and b, return the length of the longest uncommon subsequence between a and b. If the longest uncommon subsequence does not exist, return -1.
+
+        An uncommon subsequence between two strings is a string that is a subsequence of one but not the other.
+        
+        A subsequence of a string s is a string that can be obtained after deleting any number of characters from s.
+        
+        For example, "abc" is a subsequence of "aebdc" because you can delete the underlined characters in "aebdc" to get "abc". Other subsequences of "aebdc" include "aebdc", "aeb", and "" (empty string).`,
+        codeOutput: 
+        `var findLUSlength = function(a, b) {
+            //All that is required here is to return the longer string length OR the length of both (if they aren't equal)
+            //Think of it this way as an example: 'morefun' will always itself be a longer subsequence than 'fun'
+            return a === b ?
+                -1 :
+                a.length > b.length ? 
+                a.length : 
+                b.length;
+        };`,
+        code: function findLUSlength(a, b) {
+            //All that is required here is to return the longer string length OR the length of both (if they aren't equal)
+            //Think of it this way as an example: 'morefun' will always itself be a longer subsequence than 'fun'
+            return a === b ?
+                -1 :
+                a.length > b.length ? 
+                a.length : 
+                b.length;
+        },
+        arguments: {
+            descriptions: [
+                {
+                    text: "First String",
+                    type: "String"
+                },
+                {
+                    text: "Second String",
+                    type: "String"
+                }
+            ]
+        }
+    },
     findMaxConsecutiveOnes: {
         name: `findMaxConsecutiveOnes`,
         instructions: `Given a binary array nums, return the maximum number of consecutive 1's in the array.`,
