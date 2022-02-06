4376389ad475bdb564c1dba5ada85c78b73285b6
mhans003 <michaeledwardhanson@gmail.com>
2022-02-06 12:13:07 -0800

    Added solution to isToeplitzMatrix

--- codesignal-projects-react/src/solution-code/challenges_g_j.js
+++ codesignal-projects-react/src/solution-code/challenges_g_j.js
@@ -1430,6 +1430,61 @@ const challenges_g_j = {
             ]
         }
     },
+    isToeplitzMatrix: {
+        name: `isToeplitzMatrix`,
+        instructions: `Given an m x n matrix, return true if the matrix is Toeplitz. Otherwise, return false.
+
+        A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same elements.`,
+        codeOutput: 
+        `var isToeplitzMatrix = function(matrix) {
+            //Save the elements of the last row that should be present in the next.
+            let lastRowElements = [];
+            
+            //Loop through each row and determine if valid.
+            for(let row = 0; row < matrix.length; row++) {
+                //If this is not the first row, check against the last row elements.
+                if(row > 0) {
+                    //Check the last row elements to see if they are in the correct place in the current row.
+                    for(let i = 1; i < matrix[row].length; i++) {
+                        //If we do not see the expected elements shifted over in this row, return false.
+                        if(matrix[row][i] !== lastRowElements[i - 1]) return false;
+                    }
+                }
+                //Save the current row to be used to check against the next row.
+                lastRowElements = matrix[row].slice(0, -1);
+            }
+            
+            return true;
+        };`,
+        code: function isToeplitzMatrix(matrix) {
+            //Save the elements of the last row that should be present in the next.
+            let lastRowElements = [];
+            
+            //Loop through each row and determine if valid.
+            for(let row = 0; row < matrix.length; row++) {
+                //If this is not the first row, check against the last row elements.
+                if(row > 0) {
+                    //Check the last row elements to see if they are in the correct place in the current row.
+                    for(let i = 1; i < matrix[row].length; i++) {
+                        //If we do not see the expected elements shifted over in this row, return false.
+                        if(matrix[row][i] !== lastRowElements[i - 1]) return false;
+                    }
+                }
+                //Save the current row to be used to check against the next row.
+                lastRowElements = matrix[row].slice(0, -1);
+            }
+            
+            return true;
+        },
+        arguments: {
+            descriptions: [
+                {
+                    text: "Matrix of Numbers (Format: [1,2,3,4], [5,1,2,3], [9,5,1,2])",
+                    type: "NumberArrayArray"
+                }
+            ]
+        }
+    },
     isUnstablePair: {
         name: 'isUnstablePair',
         instructions: `Some file managers sort filenames taking into account case of the letters, others compare strings as if all of the letters are of the same case. That may lead to different ways of filename ordering.
