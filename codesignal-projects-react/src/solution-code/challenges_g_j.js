f6d0a95974e41434dfe5a7b56ee36fc98bc6ccdd
mhans003 <michaeledwardhanson@gmail.com>
2022-02-07 09:48:33 -0800

    Added solution to isOneBitCharacter

--- codesignal-projects-react/src/solution-code/challenges_g_j.js
+++ codesignal-projects-react/src/solution-code/challenges_g_j.js
@@ -999,6 +999,69 @@ const challenges_g_j = {
             ]
         }
     },
+    isOneBitCharacter: {
+        name: `isOneBitCharacter`,
+        instructions: `We have two special characters:
+
+        The first character can be represented by one bit 0.
+        The second character can be represented by two bits (10 or 11).
+        Given a binary array bits that ends with 0, return true if the last character must be a one-bit character.`,
+        codeOutput: 
+        `var isOneBitCharacter = function(bits) {
+            //Put all bits into groups according to the rules (10, 11, or 0).
+            let groups = [];
+            for(let i = 0; i < bits.length; i++) {
+                let thisGroup = [];
+                if(bits[i] === 1) {
+                    //If this bit is a 1, it means the next bit must be part of this group.
+                    thisGroup.push(bits[i]);
+                    thisGroup.push(bits[i + 1]);
+                    groups.push(thisGroup);
+                    //Skip over next element, since it is part of this group.
+                    i++;
+                } else {
+                    //If this bit is a 0, it means this is on its own.
+                    thisGroup.push(bits[i]);
+                    groups.push(thisGroup);
+                }
+            }
+            //Remove the last group. If this is a single zero, return true.
+            let lastGroup = groups.pop();
+            if(lastGroup.length === 1 && lastGroup[0] === 0) return true;
+            return false;
+        };`,
+        code: function isOneBitCharacter(bits) {
+            //Put all bits into groups according to the rules (10, 11, or 0).
+            let groups = [];
+            for(let i = 0; i < bits.length; i++) {
+                let thisGroup = [];
+                if(bits[i] === 1) {
+                    //If this bit is a 1, it means the next bit must be part of this group.
+                    thisGroup.push(bits[i]);
+                    thisGroup.push(bits[i + 1]);
+                    groups.push(thisGroup);
+                    //Skip over next element, since it is part of this group.
+                    i++;
+                } else {
+                    //If this bit is a 0, it means this is on its own.
+                    thisGroup.push(bits[i]);
+                    groups.push(thisGroup);
+                }
+            }
+            //Remove the last group. If this is a single zero, return true.
+            let lastGroup = groups.pop();
+            if(lastGroup.length === 1 && lastGroup[0] === 0) return true;
+            return false;
+        },
+        arguments: {
+            descriptions: [
+                {
+                    text: "Array of 1s and 0s, built of only 10, 11, or 0 (e.g. 1,1,1,0,0)",
+                    type: "NumberArray"
+                }
+            ]
+        }
+    },
     isPalindrome: {
         name: `isPalindrome`,
         instructions: `A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
