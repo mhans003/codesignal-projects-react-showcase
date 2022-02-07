d2e5367214132e99dcef9a0fb3c28b4471c40d36
mhans003 <michaeledwardhanson@gmail.com>
2022-02-07 14:59:16 -0800

    Added solution to calPoints

--- codesignal-projects-react/src/solution-code/challenges.js
+++ codesignal-projects-react/src/solution-code/challenges.js
@@ -1896,6 +1896,71 @@ const challenges = {
             ]
         }
     },
+    calPoints: {
+        name: `calPoints`,
+        instructions: `You are keeping score for a baseball game with strange rules. The game consists of several rounds, where the scores of past rounds may affect future rounds' scores.
+
+        At the beginning of the game, you start with an empty record. You are given a list of strings ops, where ops[i] is the ith operation you must apply to the record and is one of the following:
+        
+        An integer x - Record a new score of x.
+        "+" - Record a new score that is the sum of the previous two scores. It is guaranteed there will always be two previous scores.
+        "D" - Record a new score that is double the previous score. It is guaranteed there will always be a previous score.
+        "C" - Invalidate the previous score, removing it from the record. It is guaranteed there will always be a previous score.
+        Return the sum of all the scores on the record.`,
+        codeOutput: 
+        `var calPoints = function(ops) {
+            let record = [];
+            
+            ops.forEach(op => {
+                if(parseInt(op)) {
+                    //If this is a number, add new score to record in number form.
+                    record.push(parseInt(op));
+                } else if(op === "C") {
+                    //If this is a C, clear the previous score.
+                    record.pop();
+                } else if(op === "D") {
+                    //If this is a D, double the last score and add it.
+                    record.push(parseInt(record[record.length - 1] * 2));
+                } else if(op === "+") {
+                    //If this is a +, combine the last two scores.
+                    record.push(parseInt(record[record.length - 1] + record[record.length - 2]));
+                }
+            });
+            
+            //Add up the records.    
+            return record.reduce((a, b) => a += b, 0);
+        };`,
+        code: function calPoints(ops) {
+            let record = [];
+            
+            ops.forEach(op => {
+                if(parseInt(op)) {
+                    //If this is a number, add new score to record in number form.
+                    record.push(parseInt(op));
+                } else if(op === "C") {
+                    //If this is a C, clear the previous score.
+                    record.pop();
+                } else if(op === "D") {
+                    //If this is a D, double the last score and add it.
+                    record.push(parseInt(record[record.length - 1] * 2));
+                } else if(op === "+") {
+                    //If this is a +, combine the last two scores.
+                    record.push(parseInt(record[record.length - 1] + record[record.length - 2]));
+                }
+            });
+            
+            //Add up the records.    
+            return record.reduce((a, b) => a += b, 0);
+        },
+        arguments: {
+            descriptions: [
+                {
+                    text: "Array in format according to instructions (e.g. 5,2,C,D,+)",
+                    type: "StringArray"
+                }
+            ]
+        }
+    },
     canConstruct: {
         name: `canConstruct`,
         instructions: `Given two stings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
