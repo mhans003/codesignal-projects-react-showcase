6f4ef24e36c9fea0ed8d0aec9a4b4e4c1f015978
mhans003 <michaeledwardhanson@gmail.com>
2022-03-02 05:24:09 -0800

    Added solution to prefixesDivBy5

--- codesignal-projects-react/src/solution-code/challenges_o_r.js
+++ codesignal-projects-react/src/solution-code/challenges_o_r.js
@@ -532,6 +532,64 @@ const challenges_o_r = {
             ]
         }
     },
+    prefixesDivBy5: {
+        name: `prefixesDivBy5`,
+        instructions: `You are given a binary array nums (0-indexed).
+
+        We define xi as the number whose binary representation is the subarray nums[0..i] (from most-significant-bit to least-significant-bit).
+        
+        For example, if nums = [1,0,1], then x0 = 1, x1 = 2, and x2 = 5.
+        Return an array of booleans answer where answer[i] is true if xi is divisible by 5.`,
+        codeOutput: 
+        `var prefixesDivBy5 = function(nums) {
+            //Store booleans to return.
+            let bools = new Array(nums.length).fill(false);
+            
+            //Store the integer value of the 'current' binary string built from original nums array.
+            //Use BigInt for very large array inputs.
+            let current = BigInt(0);
+            
+            //Create all integer values from beginning to end.
+            for(let i = 0; i < nums.length; i++) {
+                //Since this bit is being added, it shifts over all existing bits to left, so multiply value by 2.
+                //If this bit is 1, add 1 to the value (it won't add anything if this bit is 0).
+                current = (current * 2n) + BigInt(nums[i]);
+                
+                //If this value divides by 5 with no remainder, change the corresponding boolean to true.
+                if(!(current % 5n)) bools[i] = true;
+            }
+            
+            return bools;
+        };`,
+        code: function prefixesDivBy5(nums) {
+            //Store booleans to return.
+            let bools = new Array(nums.length).fill(false);
+            
+            //Store the integer value of the 'current' binary string built from original nums array.
+            //Use BigInt for very large array inputs.
+            let current = BigInt(0);
+            
+            //Create all integer values from beginning to end.
+            for(let i = 0; i < nums.length; i++) {
+                //Since this bit is being added, it shifts over all existing bits to left, so multiply value by 2.
+                //If this bit is 1, add 1 to the value (it won't add anything if this bit is 0).
+                current = (current * 2n) + BigInt(nums[i]);
+                
+                //If this value divides by 5 with no remainder, change the corresponding boolean to true.
+                if(!(current % 5n)) bools[i] = true;
+            }
+            
+            return bools;
+        },
+        arguments: {
+            descriptions: [
+                {
+                    text: "Array of 1s and 0s (e.g. 1,0,0,1)",
+                    type: "NumberArray"
+                }
+            ]
+        }
+    },
     properNounCorrection: {
         name: 'properNounCorrection',
         instructions: `Proper nouns always begin with a capital letter, followed by small letters. Correct a given proper noun so that it fits this statement.`,
