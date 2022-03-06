061f64330c3bd916b22862a18939b63d4f52db2b
mhans003 <michaeledwardhanson@gmail.com>
2022-03-06 08:42:04 -0800

    Added solution to isMonotonic

--- codesignal-projects-react/src/pages/Solutions.js
+++ codesignal-projects-react/src/pages/Solutions.js
@@ -137,6 +137,7 @@ const Solutions = () => {
                         <option value="isIPv4Address">Is IPv4 Address</option>
                         <option value="isLucky">Is Lucky</option>
                         <option value="isMAC48Address">Is MAC48 Address</option>
+                        <option value="isMonotonic">Is Monotonic</option>
                         <option value="isNumberPalindrome">Is Number Palindrome</option>
                         <option value="isOneBitCharacter">Is One Bit Character</option>
                         <option value="isPalindrome">Is Palindrome</option>
--- codesignal-projects-react/src/solution-code/challenges_g_j.js
+++ codesignal-projects-react/src/solution-code/challenges_g_j.js
@@ -999,6 +999,63 @@ const challenges_g_j = {
             ]
         }
     },
+    isMonotonic: {
+        name: `isMonotonic`,
+        instructions: `An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+        An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
+        
+        Given an integer array nums, return true if the given array is monotonic, or false otherwise.`,
+        codeOutput: 
+        `var isMonotonic = function(nums) {
+            //Keep track of the first difference found (whether positive or negative)
+            let difference;
+            
+            for(let i = 1; i < nums.length; i++) {
+                //See if this difference is positive or negative (will be used to determine if consistent with main difference)
+                let thisDifference = nums[i] - nums[i - 1];
+                //If no difference is yet stored, save the current difference.
+                if(!difference) {
+                    difference = thisDifference;
+                } else {
+                    //If the current difference is not the same polarity (neg/pos) as the main difference, return false.
+                    if(!((difference >= 0 && thisDifference >= 0) || (difference <= 0 && thisDifference <= 0))) {
+                        return false;
+                    }
+                }
+            }
+            
+            return true;
+        };`,
+        code: function isMonotonic(nums) {
+            //Keep track of the first difference found (whether positive or negative)
+            let difference;
+            
+            for(let i = 1; i < nums.length; i++) {
+                //See if this difference is positive or negative (will be used to determine if consistent with main difference)
+                let thisDifference = nums[i] - nums[i - 1];
+                //If no difference is yet stored, save the current difference.
+                if(!difference) {
+                    difference = thisDifference;
+                } else {
+                    //If the current difference is not the same polarity (neg/pos) as the main difference, return false.
+                    if(!((difference >= 0 && thisDifference >= 0) || (difference <= 0 && thisDifference <= 0))) {
+                        return false;
+                    }
+                }
+            }
+            
+            return true;
+        },
+        arguments: {
+            descriptions: [
+                {
+                    text: "Array of Integers (e.g. 1,2,2,3,4)",
+                    type: "NumberArray"
+                }
+            ]
+        }
+    },
     isNumberPalindrome: {
         name: `isNumberPalindrome`,
         instructions: `Given an integer x, return true if x is palindrome integer.
