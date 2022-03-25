a3d867270484a5b5a5a586efde400efaae8f13aa
mhans003 <michaeledwardhanson@gmail.com>
2022-03-25 14:19:29 -0700

    Added solution to divide

--- codesignal-projects-react/src/pages/Solutions.js
+++ codesignal-projects-react/src/pages/Solutions.js
@@ -104,6 +104,7 @@ const Solutions = () => {
                         <option value="digitDifferenceSort">Digit Difference Sort</option>
                         <option value="digitsProduct">Digits Product</option>
                         <option value="distributeCandies">DistributeCandies</option>
+                        <option value="divide">Divide</option>
                         <option value="drawRectangle">Draw Rectangle</option>
                         <option value="electionsWinners">Elections Winners</option>
                         <option value="equalPairOfBits">Equal Pair of Bits</option>
--- codesignal-projects-react/src/solution-code/challenges_d_f.js
+++ codesignal-projects-react/src/solution-code/challenges_d_f.js
@@ -826,6 +826,115 @@ const challenges_d_f = {
             ]
         }
     },
+    divide: {
+        name: `divide`,
+        instructions: `Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+
+        The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would be truncated to 8, and -2.7335 would be truncated to -2.
+        
+        Return the quotient after dividing dividend by divisor.
+        
+        Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−2^31, 2^31 − 1]. For this problem, if the quotient is strictly greater than 2^31 - 1, then return 2^31 - 1, and if the quotient is strictly less than -2^31, then return -2^31.`,
+        codeOutput: 
+        `var divide = function(dividend, divisor) {
+            //Split up the numbers of the dividend into an array (keep as string for easy concatenation; Conversions to number happen after)
+            let dividendArray = Math.abs(dividend).toString().split("");
+            //Ignore negatives for the bulk of the logic.
+            let absDivisor = Math.abs(divisor);
+            //Save if quotient will be negative for after logic.
+            let isNeg = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0) ? true : false;
+            //Store quotient as individual digits.
+            let quotient = [];
+            
+            //As long as the divisor is less/equal to the number LEFT to be divided, OR there are extra remainders to deal with (this will prevent missing 0s at the end of quotient from occurring)
+            while(absDivisor <= dividendArray.map(digit => Number(digit)).join("") || dividendArray.length > 1) {
+                //Used to find/store this digit of the quotient (this iteration only gives one single digit quotient).
+                let current = 0;
+                let quotientDigit = 0;
+                
+                //Get the number to divide in this iteration.
+                let currentDividend = Number(dividendArray[0]) < absDivisor ? 
+                    Number(dividendArray.shift() + dividendArray.shift()) :
+                    Number(dividendArray.shift());
+                
+                //Get this quotient digit (counting up instead of dividing)       
+                while(current + absDivisor <= currentDividend) {
+                    quotientDigit++;
+                    current += absDivisor;
+                }
+                
+                //Save this quotient digit.
+                quotient.push(quotientDigit);
+                
+                //Push the remainder back into the beginning of the dividendArray (for next step in division).
+                dividendArray.unshift((currentDividend - current));      
+            }
+            //Deal with negative is necessary.
+            let result = isNeg ? -(Number(quotient.join(""))) : Number(quotient.join(""));
+            
+            //Deal with numbers out of bounds.
+            return result > 2147483647 ? 
+                2147483647 :
+                result < -2147483648 ?
+                -2147483648 :
+                result; 
+        };`,
+        code: function divide(dividend, divisor) {
+            //Split up the numbers of the dividend into an array (keep as string for easy concatenation; Conversions to number happen after)
+            let dividendArray = Math.abs(dividend).toString().split("");
+            //Ignore negatives for the bulk of the logic.
+            let absDivisor = Math.abs(divisor);
+            //Save if quotient will be negative for after logic.
+            let isNeg = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0) ? true : false;
+            //Store quotient as individual digits.
+            let quotient = [];
+            
+            //As long as the divisor is less/equal to the number LEFT to be divided, OR there are extra remainders to deal with (this will prevent missing 0s at the end of quotient from occurring)
+            while(absDivisor <= dividendArray.map(digit => Number(digit)).join("") || dividendArray.length > 1) {
+                //Used to find/store this digit of the quotient (this iteration only gives one single digit quotient).
+                let current = 0;
+                let quotientDigit = 0;
+                
+                //Get the number to divide in this iteration.
+                let currentDividend = Number(dividendArray[0]) < absDivisor ? 
+                    Number(dividendArray.shift() + dividendArray.shift()) :
+                    Number(dividendArray.shift());
+                
+                //Get this quotient digit (counting up instead of dividing)       
+                while(current + absDivisor <= currentDividend) {
+                    quotientDigit++;
+                    current += absDivisor;
+                }
+                
+                //Save this quotient digit.
+                quotient.push(quotientDigit);
+                
+                //Push the remainder back into the beginning of the dividendArray (for next step in division).
+                dividendArray.unshift((currentDividend - current));      
+            }
+            //Deal with negative is necessary.
+            let result = isNeg ? -(Number(quotient.join(""))) : Number(quotient.join(""));
+            
+            //Deal with numbers out of bounds.
+            return result > 2147483647 ? 
+                2147483647 :
+                result < -2147483648 ?
+                -2147483648 :
+                result; 
+        },
+        arguments: {
+            descriptions: [
+                {
+                    text: "Whole Number Dividend",
+                    type: "Number"
+                },
+                {
+                    text: "Whole Number Divisor",
+                    type: "Number"
+                }
+            ]
+        }
+    },
     drawRectangle: {
         name: 'drawRectangle',
         instructions: `You are implementing a command-line version of the Paint app. Since the command line doesn't support colors, you are using different characters to represent pixels. Your current goal is to support rectangle x1 y1 x2 y2 operation, which draws a rectangle that has an upper left corner at (x1, y1) and a lower right corner at (x2, y2). Here the x-axis points from left to right, and the y-axis points from top to bottom.
