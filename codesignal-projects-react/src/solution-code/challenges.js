1f3ffd69f7f74d64a79e2616eec514ade8e274b3
mhans003 <michaeledwardhanson@gmail.com>
2022-03-29 17:25:06 -0700

    Added solution to countPoints

--- codesignal-projects-react/src/solution-code/challenges.js
+++ codesignal-projects-react/src/solution-code/challenges.js
@@ -4596,6 +4596,65 @@ const challenges = {
             ]
         }
     },
+    countPoints: {
+        name: `countPoints`,
+        instructions: `There are n rings and each ring is either red, green, or blue. The rings are distributed across ten rods labeled from 0 to 9.
+
+        You are given a string rings of length 2n that describes the n rings that are placed onto the rods. Every two characters in rings forms a color-position pair that is used to describe each ring where:
+        
+        The first character of the ith pair denotes the ith ring's color ('R', 'G', 'B').
+        The second character of the ith pair denotes the rod that the ith ring is placed on ('0' to '9').
+        For example, "R3G2B1" describes n == 3 rings: a red ring placed onto the rod labeled 3, a green ring placed onto the rod labeled 2, and a blue ring placed onto the rod labeled 1.
+        
+        Return the number of rods that have all three colors of rings on them.`,
+        codeOutput: 
+        `var countPoints = function(rings) {
+            let indexColors = {};
+            
+            for(let i = 1; i < rings.length; i += 2) {
+                //Create a color array for this number or add to existing one
+                if(indexColors[rings[i]]) {
+                    //See if this color needs to be added to this index.
+                    if(!indexColors[rings[i]].includes(rings[i - 1])) {
+                        indexColors[rings[i]].push(rings[i - 1]);
+                    }
+                } else {
+                    //Otherwise, create the array.
+                    indexColors[rings[i]] = [rings[i - 1]];
+                }
+            }
+            
+            //Using the object that now contains arrays organized by index, return the number of them with all 3 colors.
+            return Object.values(indexColors).reduce((prev, current) => current.length === 3 ? prev + 1 : prev, 0);
+        };`,
+        code: function countPoints(rings) {
+            let indexColors = {};
+            
+            for(let i = 1; i < rings.length; i += 2) {
+                //Create a color array for this number or add to existing one
+                if(indexColors[rings[i]]) {
+                    //See if this color needs to be added to this index.
+                    if(!indexColors[rings[i]].includes(rings[i - 1])) {
+                        indexColors[rings[i]].push(rings[i - 1]);
+                    }
+                } else {
+                    //Otherwise, create the array.
+                    indexColors[rings[i]] = [rings[i - 1]];
+                }
+            }
+            
+            //Using the object that now contains arrays organized by index, return the number of them with all 3 colors.
+            return Object.values(indexColors).reduce((prev, current) => current.length === 3 ? prev + 1 : prev, 0);
+        },
+        arguments: {
+            descriptions: [
+                {
+                    text: "String exactly according to instructions (sample: B0B6G0R6R0R6G9)",
+                    type: "String"
+                }
+            ]
+        }
+    },
     countSumOfTwoRepresentations2: {
         name: 'countSumOfTwoRepresentations2',
         instructions: `Given integers n, l and r, find the number of ways to represent n as a sum of two integers A and B such that l ≤ A ≤ B ≤ r.`,
