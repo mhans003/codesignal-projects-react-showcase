34faae4ffc87079a3b4dd18966c12cbb35653b2c
mhans003 <michaeledwardhanson@gmail.com>
2022-04-16 17:49:58 -0700

    Added solution to mostVisited

--- codesignal-projects-react/src/pages/Solutions.js
+++ codesignal-projects-react/src/pages/Solutions.js
@@ -204,6 +204,7 @@ const Solutions = () => {
                         <option value="mirrorBits">Mirror Bits</option>
                         <option value="missedClasses">Missed Classes</option>
                         <option value="mostFrequentDigitSum">Most Frequent Digit Sum</option>
+                        <option value="mostVisited">Most Visited</option>
                         <option value="newNumeralSystem">New Numeral System</option>
                         <option value="newYearCelebrations">New Year Celebrations</option>
                         <option value="noIfsNoButs">No Ifs No Buts</option>
--- codesignal-projects-react/src/solution-code/challenges_k_n.js
+++ codesignal-projects-react/src/solution-code/challenges_k_n.js
@@ -1953,6 +1953,125 @@ const challenges_k_n = {
             ]
         }
     },
+    mostVisited: {
+        name: `mostVisited`,
+        instructions: `Given an integer n and an integer array rounds. We have a circular track which consists of n sectors labeled from 1 to n. A marathon will be held on this track, the marathon consists of m rounds. The ith round starts at sector rounds[i - 1] and ends at sector rounds[i]. For example, round 1 starts at sector rounds[0] and ends at sector rounds[1]
+
+        Return an array of the most visited sectors sorted in ascending order.
+        
+        Notice that you circulate the track in ascending order of sector numbers in the counter-clockwise direction (See the first example).`,
+        codeOutput: 
+        `var mostVisited = function(n, rounds) {
+            //Keep track of visited rounds.
+            let visited = {};
+            
+            for(let i = 0; i < rounds.length - 1; i++) {
+                let roundStart = rounds[i];
+                let roundEnd = rounds[i + 1];
+                
+                //Count first sector on start.
+                if(i === 0) visited[rounds[0]] = 1;
+                
+                //Count the sectors visited in this round
+                if(roundStart <= roundEnd) {
+                    let thisSector = roundStart + 1;
+                    
+                    while(thisSector <= roundEnd) {
+                        //Count every sector in between the range
+                        if(visited[thisSector]) {
+                            visited[thisSector]++;
+                        } else {
+                            visited[thisSector] = 1;
+                        }
+                        
+                        thisSector++;
+                    }
+                } else {
+                    let thisSector = 1;
+                    
+                    while(thisSector <= n) {
+                        //Include this sector if in the correct range.
+                        if(thisSector > roundStart || thisSector <= roundEnd) {
+                            if(visited[thisSector]) {
+                                visited[thisSector]++;
+                            } else {
+                                visited[thisSector] = 1;
+                            }
+                        }
+                        
+                        thisSector++;
+                    }
+                }
+            }
+                
+            //Find most frequently visited number.
+            let mostVisits = Math.max(...Object.values(visited));
+            
+            //Return the sector numbers visited the mostVisits number of times.
+            return Object.keys(visited).filter(key => visited[key] === mostVisits).map(key => Number(key)).sort((a, b) => a - b);
+        };`,
+        code: function mostVisited(n, rounds) {
+            //Keep track of visited rounds.
+            let visited = {};
+            
+            for(let i = 0; i < rounds.length - 1; i++) {
+                let roundStart = rounds[i];
+                let roundEnd = rounds[i + 1];
+                
+                //Count first sector on start.
+                if(i === 0) visited[rounds[0]] = 1;
+                
+                //Count the sectors visited in this round
+                if(roundStart <= roundEnd) {
+                    let thisSector = roundStart + 1;
+                    
+                    while(thisSector <= roundEnd) {
+                        //Count every sector in between the range
+                        if(visited[thisSector]) {
+                            visited[thisSector]++;
+                        } else {
+                            visited[thisSector] = 1;
+                        }
+                        
+                        thisSector++;
+                    }
+                } else {
+                    let thisSector = 1;
+                    
+                    while(thisSector <= n) {
+                        //Include this sector if in the correct range.
+                        if(thisSector > roundStart || thisSector <= roundEnd) {
+                            if(visited[thisSector]) {
+                                visited[thisSector]++;
+                            } else {
+                                visited[thisSector] = 1;
+                            }
+                        }
+                        
+                        thisSector++;
+                    }
+                }
+            }
+                
+            //Find most frequently visited number.
+            let mostVisits = Math.max(...Object.values(visited));
+            
+            //Return the sector numbers visited the mostVisits number of times.
+            return Object.keys(visited).filter(key => visited[key] === mostVisits).map(key => Number(key)).sort((a, b) => a - b);
+        },
+        arguments: {
+            descriptions: [
+                {
+                    text: "Whole number integer n within range",
+                    type: "Number"
+                },
+                {
+                    text: "Array of integers for rounds (e.g 1,3,1,2)",
+                    type: "NumberArray"
+                }
+            ]
+        }
+    },
     newNumeralSystem: {
         name: 'newNumeralSystem',
         instructions: `Your Informatics teacher at school likes coming up with new ways to help you understand the material. When you started studying numeral systems, he introduced his own numeral system, which he's convinced will help clarify things. His numeral system has base 26, and its digits are represented by English capital letters - A for 0, B for 1, and so on.
