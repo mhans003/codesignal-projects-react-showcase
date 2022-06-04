23589afcd9c4e46491cd2ddf0ca7214914f05afa
mhans003 <michaeledwardhanson@gmail.com>
2022-06-04 08:38:35 -0700

    Deploy updated version

--- codesignal-projects-react/src/App.js
+++ codesignal-projects-react/src/App.js
@@ -7,12 +7,14 @@ function App() {
   return (
     <Router>
       <Switch>
-        {/*
+        
         <Route exact path="/codesignal-projects-react-showcase" component={Landing}/>
         <Route exact path="/solutions/codesignal-projects-react-showcase" component={Solutions}/>
-        */}
+        
+        {/*
         <Route exact path="/" component={Landing}/>
         <Route exact path="/solutions" component={Solutions}/>
+        */}
         
       </Switch>
     </Router>
--- codesignal-projects-react/src/components/Nav.js
+++ codesignal-projects-react/src/components/Nav.js
@@ -4,8 +4,8 @@
 const Nav = () => {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-info py-4">
-            {/*<a className="navbar-brand ml-3" href="/codesignal-projects-react-showcase">Algorithm Solutions</a>*/}
-            <a className="navbar-brand ml-3" href="/">Algorithm Solutions</a>
+            <a className="navbar-brand ml-3" href="/codesignal-projects-react-showcase">Algorithm Solutions</a>
+            {/*<a className="navbar-brand ml-3" href="/">Algorithm Solutions</a>*/}
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
@@ -15,8 +15,8 @@ const Nav = () => {
                         <a className="nav-link text-light" href="https://mhans003.github.io/portfolio/portfolio.html" target="_blank" rel="noopener noreferrer">Michael Hanson</a>
                     </li>
                     <li className="nav-item">
-                        {/*<a className="nav-link text-light" href="/codesignal-projects-react-showcase">Home</a>*/}
-                        <a className="nav-link text-light" href="/">Home</a>
+                        <a className="nav-link text-light" href="/codesignal-projects-react-showcase">Home</a>
+                        {/*<a className="nav-link text-light" href="/">Home</a>*/}
                     </li>
                 </ul>
             </div>
